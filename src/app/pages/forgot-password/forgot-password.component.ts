1d688057e4fd330c028894d7b476bad91b63ad63
Whatley, Cesar <julceswhat@gmail.com>
2020-02-01 02:55:50 -0500

    Updating signup and forgot password pages ui

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
@@ -8,9 +9,28 @@ import { AuthService } from '../../core/services/auth.service';
 })
 export class ForgotPasswordComponent implements OnInit {
 
+    public forgotPasswordForm: FormGroup;
+
     constructor(public authService: AuthService) { }
 
     ngOnInit() {
+        this.setupForm();
     }
 
+    public setupForm() {
+        this.forgotPasswordForm = new FormGroup({
+            email: new FormControl('', [
+                Validators.required,
+                Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\\.[a-zA-Z]{2,15}$')
+            ])
+        });
+    }
+
+    public resetPassword() {
+        this.authService.forgotPassword(this.forgotPasswordForm.value.email)
+    }
+
+    public isFormInvalid(field: string) {
+        return (this.forgotPasswordForm.get(field).invalid && this.forgotPasswordForm.get(field).touched);
+    }
 }
